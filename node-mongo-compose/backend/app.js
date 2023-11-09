6b4bc1de94dde9128db562dc499e75a1a58dd7e5
denesdonato <denes.rafael@gmail.com>
2023-11-09 14:56:13 -0300

    Docker Compose - Aplicação

--- node-mongo-compose/backend/app.js
+++ node-mongo-compose/backend/app.js
@@ -1,15 +1,35 @@
 const express = require("express")
-const restul = require("node-restful")
+const restful = require("node-restful")
 
 const server = express()
-const mongoose = restul.mongoose
+const mongoose = restful.mongoose
+
+const bodyParser = require("body-parser")
+const cors = require("cors")
 
 // Database
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://db/mydb')
 
 // Teste
-server.get('/', (req, res, next) => res.send('Backend'))
+//server.get('/', (req, res, next) => res.send('Backend'))
+
+// Middlewares
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+server.use(cors())
+
+// ODM
+const Client = restful.model('Client', {
+    name: { type: String, required: true }
+})
+
+// Rest API
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({new: true, runValidators: true})
+
+// Routes
+Client.register(server, '/clients')
 
 // Servidor
 server.listen(3000)
